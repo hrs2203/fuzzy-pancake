eedd578e62ee06152992817cf5a72b1a00f7aa59
hrs2203 <hrishabh2203@gmail.com>
2021-01-10 15:25:50 +0530

    2341sample_present

--- frontend/queryComparisoinMain.js
+++ frontend/queryComparisoinMain.js
@@ -1,12 +1,40 @@
-sampleData = {
-    "inputQuery1": "Lorem Ipsem , Lorem Ipsem , Lorem Ipsem , Lorem Ipsem ,",
-    "inputQuery2": "Lorem Ipsem , Lorem Ipsem , Lorem Ipsem , Lorem Ipsem ,",
-    "comparisonMetrics": {
-        "SemanticSimilarity": 50.0,
-        "DoTheyAgree": true,
-        "otherKindOfSimilarity": 60,
+// d = {
+//     "inputQuery1": "Lorem Ipsem , Lorem Ipsem , Lorem Ipsem , Lorem Ipsem ,",
+//     "inputQuery2": "Lorem Ipsem , Lorem Ipsem , Lorem Ipsem , Lorem Ipsem ,",
+//     "comparisonMetrics": {
+//         "SemanticSimilarity": 50.0,
+//         "DoTheyAgree": true,
+//         "otherKindOfSimilarity": 60,
+//     }
+// }
+
+// outputBlock.innerHTML = generateCompareTable(responseQuery);
+
+
+function getQueryResponse(q1, q2) {
+
+    requestBody = {
+        "inputQuery1": q1,
+        "inputQuery2": q2
     }
+
+    var settings = {
+        "url": "http://hrs2203.pythonanywhere.com/compareQuery",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "data": JSON.stringify({ "inputQuery1": q1, "inputQuery2": q2 }),
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        outputBlock.innerHTML = generateCompareTable(JSON(response));
+    });
+
 }
+
 
 function generateCompareTable(inputData) {
     var contentBody = '<table class="table">'
@@ -42,24 +70,18 @@ function compareClickEvent() {
 
     queryBlock2 = document.getElementById('query2');
     query2 = queryBlock2.value;
-    
-    outputBlock = document.getElementById("outputBlock");
-    
 
-    if (query1.length == 0 || query2.length == 0 ) {
+    outputBlock = document.getElementById("outputBlock");
+
+
+    if (query1.length == 0 || query2.length == 0) {
         outputBlock.innerHTML = `<p>detected empty query.<p>`
     } else {
-
-        sampleData["inputQuery1"] = query1;
-        sampleData["inputQuery2"] = query2;
-
         // ======== informing user ===========
         outputBlock.innerHTML = `<p>Comparing Queries Please Wait...<p>`
 
-        setTimeout(function () {
-            // ======== loadind response =========
-            outputBlock.innerHTML = generateCompareTable(sampleData);
-        }, 3000);
+        getQueryResponse(query1, query2);
+
     }
 
 }
