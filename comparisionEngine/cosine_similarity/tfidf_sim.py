2063f9ee47211e43544a54e7c394b2980cd312e0
hrs2203 <hrishabh2203@gmail.com>
2021-04-03 17:09:37 +0530

    tfidf works

--- comparisionEngine/cosine_similarity/tfidf_sim.py
+++ comparisionEngine/cosine_similarity/tfidf_sim.py
@@ -1,6 +1,15 @@
 import numpy as np
-from stop_words import nltk_stop_words as stop_words
+from comparisionEngine.cosine_similarity.stop_words import nltk_stop_words as stop_words
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize 
+
+# On first executino uncomment this
+# import nltk
+# nltk.download('punkt')
+# nltk.download('wordnet')
 
 class LemmaTokenizer:
     """ Interface to the WordNet lemmatizer from nltk """
@@ -17,28 +26,26 @@ class LemmaTokenizer:
             if t not in self.ignore_tokens
         ]
 
-
-tokenizer = LemmaTokenizer()
-
-
-vectorizer = TfidfVectorizer(stop_words=stop_words, tokenizer=tokenizer)
+class TfidfSim:
+    tokenizer = LemmaTokenizer()
+    vectorizer = TfidfVectorizer(stop_words=stop_words, tokenizer=tokenizer)
 
 
-def rank_documents(search_terms: str, documents: list):
-    """ Search for search_terms in documents, and return a list of cosine similarity scores """
+    def rank_documents(self, search_terms: str, documents: list):
+        """ Search for search_terms in documents, and return a list of cosine similarity scores """
 
-    try:
-        vectors = vectorizer.fit_transform([search_terms] + documents)
+        try:
+            vectors = self.vectorizer.fit_transform([search_terms] + documents)
 
-        cosine_similarities = linear_kernel(vectors[0:1], vectors).flatten()
+            cosine_similarities = linear_kernel(vectors[0:1], vectors).flatten()
 
-        document_scores = [
-            item.item() for item in cosine_similarities[1:]
-        ]  # convert back to native Python dtypes
+            document_scores = [
+                item.item() for item in cosine_similarities[1:]
+            ]  # convert back to native Python dtypes
 
-    except ValueError:
-        print(f"Unable to rank documents for search terms: {search_terms}")
+        except ValueError:
+            print(f"Unable to rank documents for search terms: {search_terms}")
 
-        document_scores = [0.0 for _ in range(len(documents))]
+            document_scores = [0.0 for _ in range(len(documents))]
 
-    return document_scores
+        return document_scores
--- sample.py
+++ sample.py
@@ -1,8 +1,18 @@
 """Sample Implimentation of approacher"""
 
 # ===== Count Similarity =====
-from comparisionEngine.cosine_similarity.word_count_sim import WordCountSim
-compEngine = WordCountSim()
-comp_val = compEngine.wordSim("people of earth are good","people of earth are cold")
-print("simple comparision metric : {}".format(comp_val))
+# from comparisionEngine.cosine_similarity.word_count_sim import WordCountSim
+# compEngine = WordCountSim()
+# comp_val = compEngine.wordSim("people of earth are good","people of earth are cold")
+# print("simple comparision metric : {}".format(comp_val))
 
+
+# ======== Tf-Idf Sim ===========
+from comparisionEngine.cosine_similarity.tfidf_sim import TfidfSim
+
+compEngine = TfidfSim()
+res = compEngine.rank_documents(
+    "the earth is good",
+    ["this is mars", "this is good place", "earth is nice", "earth is not nice"],
+)
+print(res)
